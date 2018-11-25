cf74450d1a117c4bcbc3f68d7cee13dc3d080588
Hatim Hakimjee <hatimh@criticalmass.com>
2018-11-25 13:58:40 -0700

    [feature/user-authenticatio] Rename app to abide by github app name policies.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get root" do
+    get root_path
+    assert_response :success
+    assert_select '.navbar-brand', 'SnippetMan'
+  end
 end
